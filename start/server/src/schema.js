b8f6e3e05213a53a8595e80bf99e053603595307
Carleton Atwater <carleton@promptworks.com>
2019-08-21 16:44:46 -0400

    Define initial 'launches' query to fetch all upcoming launches Returns array of not null launches Also includes 'launch' query that will return single Launch object based on ID parameter Finally, a 'me' querry that returns data for current user

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,5 +1,15 @@
 const { gql } = require('apollo-server');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    #launches query returns not null array of Launch  objects
+    launches: [Launch]!
+    #launch query returns single Launch object based on id parameter
+    launch(id: ID!): Launch
+    # Returns User object for current user 
+    me: User
+  }
 
+
+`;
 module.exports = typeDefs;
