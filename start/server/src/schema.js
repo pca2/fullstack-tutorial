a5600d8320d63411414238cc7630e833e9178cb7
Carleton Atwater <carleton@promptworks.com>
2019-08-21 17:40:02 -0400

    Add extra comment to Query

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -3,6 +3,7 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     #launches query returns not null array of Launch  objects
+    # The bang indicates not null
     launches: [Launch]!
     #launch query returns single Launch object based on id parameter
     launch(id: ID!): Launch
